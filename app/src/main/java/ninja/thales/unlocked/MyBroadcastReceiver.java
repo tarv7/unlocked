987aa6bf89e8802fc3ece116e5c5a0a22510a84a
Thales Augusto <thallles.ramos@gmail.com>
2018-04-23 23:53:05 -0300

    Primeiro commit

---
+++ app/src/main/java/ninja/thales/unlocked/MyBroadcastReceiver.java
@@ -0,0 +1,10 @@
+package ninja.thales.unlocked;
+
+import android.content.BroadcastReceiver;
+
+/**
+ * Created by targus on 23/04/18.
+ */
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+}
